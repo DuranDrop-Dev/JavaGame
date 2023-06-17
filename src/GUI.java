d198978d72e15ba51f0faf94e56c32a714af44ab
DuranDrop-Dev <127360444+DuranDrop-Dev@users.noreply.github.com>
2023-06-16 20:21:50 -0700

    Code cleanup & comments added

--- src/GUI.java
+++ src/GUI.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 import java.awt.*;
 
 import static java.lang.System.exit;
+import static src.assets.util.Controls.updatePosition;
 import static src.assets.util.Target.removePaint;
 
 public class GUI {
@@ -21,58 +22,74 @@ public class GUI {
     public static JLabel sprite = new JLabel();
     public static JLabel currentPosition = new JLabel();
     public static Font boldFont = new Font("arial", Font.BOLD, 25);
-    public static boolean isRunning = true;
-    public static Thread g;
+    public static boolean isRunning;
+    public static Thread gameLoop;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(GUI::createAndShowGUI);
     }
 
     private static void createAndShowGUI() {
+        // Create main JFrame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 
-        playGame();
-
-        Controls.Pad(sprite);
-        sprite.setIcon(target);
-
+        // Create and set current position JLabel
         currentPosition.setFont(boldFont);
         currentPosition.setForeground(Color.white);
-        currentPosition.setBounds(20, 20, 100, 40);
+        currentPosition.setBounds((FRAME_WIDTH / 2) - 90, FRAME_HEIGHT - 90, 180, 40);
+        updatePosition();
+        contentPanel.add(currentPosition, SwingConstants.CENTER);
 
+        // Create and set target icon
+        Controls.Pad(sprite);
+        sprite.setIcon(target);
+        sprite.setBounds(75, 75, 75, 75);
+        contentPanel.add(sprite);
+
+        // Main panel setting
         mainPanel.setPreferredSize(new Dimension(GUI.FRAME_WIDTH, GUI.FRAME_HEIGHT));
         mainPanel.setLayout(new OverlayLayout(mainPanel));
         mainPanel.setBackground(Color.BLACK);
 
+        // Display JFrame
         frame.setContentPane(mainPanel);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        // Play game
+        playGame();
     }
 
     private static void playGame() {
         isRunning = true;
-        g = new Thread(() -> {
+        gameLoop = new Thread(() -> {
             synchronized (contentPanel) {
                 try {
                     while (isRunning) {
+                        // create main JPanel
                         mainPanel.add(contentPanel);
-                        mainPanel.repaint();
                         mainPanel.revalidate();
-                        System.out.println("contentPanel added");
+                        mainPanel.repaint();
 
-                        contentPanel.add(sprite);
-                        contentPanel.add(currentPosition);
-
+                        // start game
                         Target.createTarget();
+
+                        // regenerate images
+                        contentPanel.revalidate();
+                        contentPanel.repaint();
+
+                        // wait until notifyAll() is called
                         contentPanel.wait();
 
+                        // reset arrays and images
                         removePaint();
 
+                        // Keep playing option
                         int keepPlaying = JOptionPane.showConfirmDialog(null, "Play again?");
                         if (keepPlaying == JOptionPane.NO_OPTION) {
-                            JOptionPane.showMessageDialog(null, "OK");
+                            JOptionPane.showMessageDialog(null, "Goodbye!");
                             exit(0);
                         }
                     }
@@ -81,6 +98,6 @@ public class GUI {
                 }
             }
         });
-        g.start();
+        gameLoop.start();
     }
 }
