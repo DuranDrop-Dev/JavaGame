c0d30b198100264bd5df888b0674e55ecfb694ec
DuranDrop-Dev <127360444+DuranDrop-Dev@users.noreply.github.com>
2023-06-18 18:08:28 -0700

    High score record relocated

--- src/assets/util/HighScore.java
+++ src/assets/util/HighScore.java
@@ -16,7 +16,7 @@ public class HighScore {
     }
 
     private static void writeDataToFile(String firstName, String lastName, int winStreak) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("record.txt"))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/record.txt"))) {
             // Write name and winStreak to the file
             writer.write(firstName);
             writer.newLine();
@@ -29,7 +29,7 @@ public class HighScore {
     }
 
     public static DataModel readDataFromFile() {
-        try (BufferedReader reader = new BufferedReader(new FileReader("record.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/record.txt"))) {
             String firstName = reader.readLine(); // Read the firstName from the first line
             String lastName = reader.readLine(); // Read the lastName from the first line
             int winStreak = Integer.parseInt(reader.readLine()); // Read the age from the second line
