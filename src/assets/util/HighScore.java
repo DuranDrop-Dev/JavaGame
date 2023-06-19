581dded334025f6fcecb617924c29410eecf10e6
DuranDrop-Dev <127360444+DuranDrop-Dev@users.noreply.github.com>
2023-06-18 17:24:49 -0700

    last name value added to methods

--- src/assets/util/HighScore.java
+++ src/assets/util/HighScore.java
@@ -3,21 +3,24 @@ package src.assets.util;
 import java.io.*;
 
 public class HighScore {
-    public static void writeScore(String player, int wins) {
+    public static void writeScore(String firstName, String lastName, int wins) {
         // Write data to the text file
-        writeDataToFile(String.valueOf(player), wins);
+        writeDataToFile(firstName, lastName, wins);
 
         // TEST: read data from the text file
         DataModel tempModel = readDataFromFile();
         assert tempModel != null;
-        System.out.println("Name: " + tempModel.name());
+        System.out.println("First Name: " + tempModel.firstName());
+        System.out.println("Last Name: " + tempModel.lastName());
         System.out.println("Win Streak: " + tempModel.winStreak());
     }
 
-    private static void writeDataToFile(String name, int winStreak) {
+    private static void writeDataToFile(String firstName, String lastName, int winStreak) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("record.txt"))) {
             // Write name and winStreak to the file
-            writer.write(name);
+            writer.write(firstName);
+            writer.newLine();
+            writer.write(lastName);
             writer.newLine();
             writer.write(String.valueOf(winStreak));
         } catch (IOException e) {
@@ -27,15 +30,16 @@ public class HighScore {
 
     public static DataModel readDataFromFile() {
         try (BufferedReader reader = new BufferedReader(new FileReader("record.txt"))) {
-            String name = reader.readLine(); // Read the name from the first line
+            String firstName = reader.readLine(); // Read the firstName from the first line
+            String lastName = reader.readLine(); // Read the lastName from the first line
             int winStreak = Integer.parseInt(reader.readLine()); // Read the age from the second line
-            return new DataModel(name, winStreak);
+            return new DataModel(firstName, lastName, winStreak);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
         return null;
     }
 
-    public record DataModel(String name, int winStreak) {
+    public record DataModel(String firstName, String lastName, int winStreak) {
     }
 }
