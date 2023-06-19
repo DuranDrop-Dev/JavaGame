f5f61774a38f703c646ad2b574dd69cfc7cdcb4e
DuranDrop-Dev <127360444+DuranDrop-Dev@users.noreply.github.com>
2023-06-18 17:24:19 -0700

    interface renamed & default methods adjusted

--- src/assets/components/Player.java
+++ src/assets/components/Player.java
@@ -1,68 +1,14 @@
 package src.assets.components;
 
-public class Player implements PlayerStats {
-    private String firstName;
-    private String lastName;
-    private int wins;
-    public Player() {
-        firstName = "";
-        lastName = "";
-    }
-    public Player(String first) {
-        setFirstName(first);
-        setLastName("");
-    }
-    public Player(String first, String last) {
-        setFirstName(first);
-        setLastName(last);
-    }
-    public void setFirstName(String first) {
-        firstName = first;
-    }
+public interface Player {
 
-    public void setLastName(String last) {
-        lastName = last;
-    }
+    void setFirstName(String first);
 
-    public String getFirstName() {
-        return firstName;
-    }
+    String getFirstName();
 
-    public String getLastName() {
-        return lastName;
-    }
-    public void resetWinStreak() {
-        wins = 0;
-    }
-    @Override
-    public int getWinStreak() {
-        return wins;
-    }
+    void setLastName(String last);
 
-    @Override
-    public void setWinStreak() {
-        setWinStreak(0);
-    }
+    String getLastName();
 
-    @Override
-    public void setWinStreak(int winStreak) {
-        wins += winStreak;
-    }
-
-    @Override
-    public String displayPlayerAndStats() {
-        if (!getLastName().equals("")) {
-            return getFirstName() + " " + getLastName() + " ( " + getWinStreak() + " )";
-        } else {
-            return getFirstName() + " ( " + getWinStreak() + " )";
-        }
-    }
-    @Override
-    public String toString() {
-        if (!getLastName().equals("")) {
-            return getFirstName() + " " + getLastName();
-        } else {
-            return getFirstName();
-        }
-    }
+    String displayPlayerAndStats();
 }
